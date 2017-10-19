5129e91c78b71df0060bebeb503f2847540d8e4d
Nick Brazeau <nbrazeau@med.unc.edu>
2017-10-19 11:01:47 -0400

    split

---
+++ R/getLociPopAF.R
@@ -0,0 +1,43 @@
+#' @title polyIBD popAF
+#' @description .....
+#' @param file
+#' @export
+
+# TO DO
+#   Need to make this object
+
+#-----------------------------------------------
+# Calculate Pop AF per Loci 
+#----------------------------------------------
+getLociPopAF <- function(snpmatrix_rc_samploci){
+  # Parses out the Format Field in a VCF file 
+  #
+  # Args:
+  #   snpmatrix_rc_samploci: A SNP matrix of GT calls  
+  #
+  # Returns:
+  #   A 2xN matrix of population allele frequencies, p & q
+  
+  ############################
+  ##### Error handling #######
+  ############################
+  list.of.packages <- c("tidyverse")
+  new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+  if(length(new.packages)) install.packages(new.packages) # here it will install if not already installed
+  
+  # Check input
+  if(is.null(snpmatrix_rc_samploci)){
+    stop("Must input a snpmatrix that was generated using the vcf2infSNPmatrix function.")
+  }
+  
+  
+  ###################################
+  #####      CODE for AF     #######
+  ##################################
+  popAF <- matrix(rep(NA, 2*ncol(snpmatrix_rc_samploci)), nrow=2) # init the matrix
+  
+  popAF[1,] <- apply(snpmatrix_rc_samploci[2:nrow(snpmatrix_rc_samploci),], 2, function(x){(2*(length(which(x==0))) + length(which(x==1)))/(2*length(x))}) # since we know homozygous ref is 0, so this counts as 2 As, and then we count hets and then divide by 2*possible alleles. Doing this roundabout way because we aren't in HWE
+  popAF[2,] <- 1-popAF[1,] # now we have q
+  
+  return(popAF)
+}
--- R/vcf2SNPmatrix.R
+++ R/vcf2SNPmatrix.R
@@ -1,4 +1,4 @@
-#' @title polyIBD Transition Probabilities 
+#' @title polyIBD vcf2SNPmatrix
 #' @description .....
 #' @param file
 #' @export
@@ -64,78 +64,6 @@ vcf2infSNPmatrix <- function(vcffile, biallelic=T, diploid=T) {
   snpmatrix_inform <- cbind(POS, snpmatrix_inform)
   return(snpmatrix_inform)
   
-}
-
-
-
-
-#-----------------------------------------------
-# Calculate Pop AF per Loci 
-#----------------------------------------------
-getLociPopAF <- function(snpmatrix_rc_samploci){
-  # Parses out the Format Field in a VCF file 
-  #
-  # Args:
-  #   snpmatrix_rc_samploci: A SNP matrix of GT calls  
-  #
-  # Returns:
-  #   A 2xN matrix of population allele frequencies, p & q
-  
-  ############################
-  ##### Error handling #######
-  ############################
-  list.of.packages <- c("tidyverse")
-  new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
-  if(length(new.packages)) install.packages(new.packages) # here it will install if not already installed
-  
-  # Check input
-  if(is.null(snpmatrix_rc_samploci)){
-    stop("Must input a snpmatrix that was generated using the vcf2infSNPmatrix function.")
-  }
-  
-  
-  ###################################
-  #####      CODE for AF     #######
-  ##################################
-  popAF <- matrix(rep(NA, 2*ncol(snpmatrix_rc_samploci)), nrow=2) # init the matrix
-  
-  popAF[1,] <- apply(snpmatrix_rc_samploci[2:nrow(snpmatrix_rc_samploci),], 2, function(x){(2*(length(which(x==0))) + length(which(x==1)))/(2*length(x))}) # since we know homozygous ref is 0, so this counts as 2 As, and then we count hets and then divide by 2*possible alleles. Doing this roundabout way because we aren't in HWE
-  popAF[2,] <- 1-popAF[1,] # now we have q
-  
-  return(popAF)
-}
-
-
-#-----------------------------------------------
-# Calculate Genetic Distance
-#----------------------------------------------
-getLocigeneticdist <- function(snpmatrix_rc_samploci){
-  # Parses out the Format Field in a VCF file 
-  #
-  # Args:
-  #   snpmatrix_rc_samploci: A SNP matrix of GT calls  
-  #
-  # Returns:
-  #   A vector of the genetic distance between consecutive loci 
-  
-  ############################
-  ##### Error handling #######
-  ############################
-  list.of.packages <- c("tidyverse")
-  new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
-  if(length(new.packages)) install.packages(new.packages) # here it will install if not already installed
-  
-  #####################################################
-  #####      CODE for Genetic Distance by Loci #######
-  ####################################################
-  locigendist <- matrix(rep(NA, ncol(snpmatrix_rc_samploci)), nrow=1) # init the matrix
-  locigendist[1] <- NA # redundant but to be explicit
-  
-  for(i in 2:nrow(snpmatrix_rc_samploci)){
-    locigendist[i] <- snpmatrix_rc_samploci[i,1] - snpmatrix_rc_samploci[i-1,1]
-  }
-  
-  return(locigendist)
 }
 
 
