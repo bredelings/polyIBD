acf1e95c6fe26b289d6571f3197c8a5ae2eead9e
Nicholas Brazeau <nbrazeau@med.unc.edu>
2018-09-10 10:47:18 -0400

    revert k back to bob's initial coding

--- src/MCMC.cpp
+++ src/MCMC.cpp
@@ -135,8 +135,7 @@ void MCMC::burnin_MCMC(Rcpp::List args_functions) {
       if (m1==m1_prop && m2==m2_prop && f_prop!=f) {
         f_propSD  += (1-0.23)/sqrt(double(rep));
       }
-
-      // or update k_max (by doing nothing since we need irreducibility)
+      // or update k_weight_move
       if (m1==m1_prop && m2==m2_prop && k_prop!=k) {
         k_weight_move = (k==k_prop) ? k_weight_move : ++k_weight_move;
       }
@@ -169,8 +168,9 @@ void MCMC::burnin_MCMC(Rcpp::List args_functions) {
       // update k move
       if (m1==m1_prop && m2==m2_prop && k_prop!=k) {
         k_weight_stay = (k==k_prop) ? k_weight_stay : ++k_weight_stay;
+        // limit k sampling weights
+        k_weight_stay = (k_weight_stay > 100*k_weight_move) ? 100*k_weight_move : k_weight_stay;
       }
-      
     }
     // store logLike
     logLike_burnin_store[rep] = logLike_old;
@@ -658,8 +658,8 @@ double MCMC::propose_m(double m_current, double weight_move, double weight_stay)
 double MCMC::propose_k(double k_current, double weight_move, double weight_stay) {
   double k_prop = k_current;
   if (rbernoulli1( weight_move/double(weight_move + weight_stay) )) {
-    k_prop = sample2(0, k_max);
+    k_prop = rbernoulli1(0.5) ? k_current+1 : k_current-1;
+    k_prop = (k_prop==0 || k_prop>k_max) ? k_current : k_prop;
   }
-
   return(k_prop);
 }
